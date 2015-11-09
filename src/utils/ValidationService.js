38e99f14a585b858f4ec92f367c3e0c4d25fafdd
Jae Lee <jaelee125@gmail.com>
2015-11-09 03:59:44 -0600

    Delete ValidationService.js

--- src/utils/ValidationService.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-  validateInvoiceNumber: function(invoiceNumber) {
-    console.log(typeof invoiceNumber)
-    if(typeof invoiceNumber === 'number'){
-      alert("Invoice Number must be a number");
-    }
-  }
-}
